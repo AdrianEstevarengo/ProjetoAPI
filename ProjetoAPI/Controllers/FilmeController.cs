1d9108a4918b81df37e20a036dbeec39a38175be
AdrianEstevarengo <adrian18matos@gmail.com>
2024-11-19 13:08:06 -0400

    Criando projeto de API. Classe FIlme criada. metodos de inserir e retornar criado. Implementar retorno por ID.

---
+++ ProjetoAPI/Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoAPI.Models;
+using System.Data.SqlTypes;
+
+namespace ProjetoAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes()
+    {
+        return filmes;
+    }
+}
